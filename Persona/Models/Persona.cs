8f5a7566e0671ae23cfaa4cca4a02454ad7e31cc
Diego Gómez <72423267+Haruki1707@users.noreply.github.com>
2024-07-16 15:50:44 -0600

    Initial commit

---
+++ Persona/Models/Persona.cs
@@ -0,0 +1,19 @@
+﻿namespace Persona.Models
+{
+    public class Persona
+    {
+        public int ID { get; set; }
+
+        public string DUI { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Apellido { get; set; }
+
+        public DateOnly FechaNacimiento { get; set; }
+
+        public string Direccion { get; set; }
+
+        public string Correo { get; set; }
+    }
+}
